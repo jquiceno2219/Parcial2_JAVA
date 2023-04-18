877b8d374ba7d756237032ce7f9310abdfe0a45c
jquiceno2219 <119070759+jquiceno2219@users.noreply.github.com>
2023-04-18 16:19:33 -0500

    Add files via upload

--- Parcial2/src/Ejercicio2/Main.java
+++ Parcial2/src/Ejercicio2/Main.java
@@ -10,16 +10,59 @@ public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         String opt = "1";
         FuelPointsRepository fuelPoints = new FuelPointsRepoImpl();
-        do{
+        do {
             Scanner s = new Scanner(System.in);
-            System.out.println("Menu \n 1.Add \n 2.Show List \n 3.Edit List \n 4.Delete \n 5.Exit");
+            System.out.println("Welcome To the Menu: \n 1.Add \n 2.Show List \n 3.Edit List \n 4.Delete \n 5.Exit");
             opt = s.next();
-            switch(opt) {
+            switch (opt) {
                 case "1": {
 
                     System.out.println("User Id: ");
-                    Integer id = s.nextInt();
+                    int id = s.nextInt();
+                    System.out.println("User name: ");
+                    String clientName = s.next();
+                    System.out.println("Price of fuel: ");
+                    int fuelPrice = s.nextInt();
+                    System.out.println("Total pts.: ");
+                    int ptsAccum = s.nextInt();
+                    fuelPoints.save(new FuelPoints(id, clientName, fuelPrice, ptsAccum));
+                    ;
+                    break;
+
+                }
+                case "2": {
+                    List<FuelPoints> fuelPoints1 = fuelPoints.findAll();
+                    fuelPoints1.forEach(System.out::println);
+                    break;
+                }
+                case "3": {
+                    System.out.println("=====Edit=====");
+                    System.out.println("Id: ");
+                    int id = s.nextInt();
+                    System.out.println("Client Name: ");
+                    String clientName = s.next();
+                    System.out.println("Fuel Price: ");
+                    int fuelPrice = s.nextInt();
+                    System.out.println("Pts: ");
+                    int ptsAccum = s.nextInt();
+                    FuelPoints update = new FuelPoints(id, clientName, fuelPrice, ptsAccum);
+                    fuelPoints.update(update);
+                    break;
+                }
+                case "4": {
+                    System.out.println("=====Delete=====");
+                    System.out.println("Insert Id: ");
+                    int id = s.nextInt();
+                    fuelPoints.delete(fuelPoints.findById(id));
+                    fuelPoints.findAll().forEach(System.out::println);
+                    break;
+                }
+                case "5": {
+                    opt = "5";
+                    break;
+                }
 
             }
-        }
+        }while (opt != "5") ;
+    }
 }
